336308d2b0bce9104792d12c988f182d2843c6e1
stefanobrambilla <stefano.brambilla84@gmail.com>
2017-03-05 20:38:27 +0100

    redux

---
+++ src/containers/book-list.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+
+class BookList extends Component {
+
+
+    renderList(){
+
+        return this.props.books.map((book) => {
+
+            return (
+                <li
+                    key={book.title}
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title}
+                </li>
+            );
+
+        });
+
+    }
+74
+    render(){
+        console.log(this.props.activeBook);
+
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+
+        );
+
+    }
+
+
+}
+
+function mapStateToProps(state) {
+
+    return {
+        books: state.books,
+        activeBook: state.activeBook
+    };
+
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ selectBook: selectBook}, dispatch);
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
+
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function () {
+
+    return [
+        { title: 'Javascript: The Good Parts'},
+        { title: 'Harry Potters'},
+        { title: 'The dark tower'},
+        { title: 'Eloquent Ruby'}
+    ]
+
+}
